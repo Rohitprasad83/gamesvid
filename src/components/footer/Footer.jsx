e6a5a8286ee44c425ef1381385a5e8791e833910
Rohit Prasad <pdrohit674@gmail.com>
2022-04-26 01:49:41 +0530

    added footer icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,12 +3,42 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer text__md">
-      <div>Home</div>
-      <div>Explore</div>
-      <div>Playlist</div>
-      <div>Liked Videos</div>
-      <div>Watch Later</div>
-      <div>History</div>
+      <div className="footer-item">
+        <span>
+          <i className="fa-solid fa-house"></i>
+        </span>
+        Home
+      </div>
+      <div className="footer-item">
+        <span>
+          <i className="fa-solid fa-compass"></i>
+        </span>
+        Explore
+      </div>
+      <div className="footer-item">
+        <span>
+          <i class="fa-solid fa-layer-group"></i>
+        </span>
+        Playlist
+      </div>
+      <div className="footer-item">
+        <span>
+          <i className="fa-solid fa-heart"></i>
+        </span>
+        Liked Videos
+      </div>
+      <div className="footer-item">
+        <span>
+          <i className="fa-solid fa-clock"></i>
+        </span>
+        Watch Later
+      </div>
+      <div className="footer-item">
+        <span>
+          <i className="fa-solid fa-clock-rotate-left"></i>
+        </span>
+        History
+      </div>
     </div>
   )
 }
