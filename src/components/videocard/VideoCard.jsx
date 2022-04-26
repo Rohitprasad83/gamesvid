c263e7212696e3d3f39cea9720c0af3ddda28c22
Rohit Prasad <pdrohit674@gmail.com>
2022-04-27 00:19:40 +0530

    feat: improved styling for a video card

--- src/components/videocard/VideoCard.jsx
+++ src/components/videocard/VideoCard.jsx
@@ -1,20 +1,40 @@
 import React from 'react'
 import videoCard from 'assets/images/videoCard.jpg'
 import avatarVideo from 'assets/images/avatarVideo.png'
-export default function VideoCard() {
+export default function VideoCard({ video }) {
+  const {
+    _id,
+    title,
+    description,
+    views,
+    creator,
+    duration,
+    thumbnail,
+    alt,
+    category,
+    avatar,
+    link,
+  } = video
   return (
     <div className="video-card text__md">
-      <img src={videoCard} alt="Video Card" />
+      <img src={thumbnail} alt={alt} />
       <div className="video-heading">
-        <div className="avatar avatar__sm">
-          <img src={avatarVideo} alt="avatar for Video" />
+        <div className="video-avatar">
+          <img src={avatar} alt="avatar for Video" className="video-avatar" />
         </div>
-        <span className="video-title">Title for the Video</span>
+        <span className="video-title font__bold">{title}</span>
       </div>
-      <div className="video-channel">Channel</div>
+      <div className="video-channel font__regular text__md">{creator}</div>
       <div className="video-footer">
-        <span>Views</span>
-        <span>Age of the video</span>
+        <span>{views} views</span>
+        <span>
+          {' '}
+          <i className="fa-solid fa-stopwatch"></i>
+          {duration}
+        </span>
+        <span>
+          <i className="fa-solid fa-bars video-option"></i>
+        </span>
       </div>
     </div>
   )
