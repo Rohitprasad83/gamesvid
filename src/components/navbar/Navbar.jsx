e7c68dcc4c380a1f5486192aa008fd3f1b85291e
Rohit Prasad <pdrohit674@gmail.com>
2022-04-30 01:41:03 +0530

    feat: added login logout from navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,28 @@
+import { useAuth } from 'context'
 import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Navbar() {
+  const { setEncodedToken } = useAuth
+
+  const encodedToken = localStorage.getItem('token')
+  const logout = () => {
+    localStorage.removeItem('token')
+    setEncodedToken(null)
+  }
   return (
     <nav className="navbar simple">
       <h3 className="navbar__header"> GamesVid </h3>
       <ul className="navbar__list">
         <li className="navbar__list__items">
-          <span className="badge__icons">
+          {encodedToken !== null ? (
+            <Link to="/" onClick={logout}>
+              <button className="btn btn__secondary navbar-btn">Logout</button>
+            </Link>
+          ) : (
             <Link to="/login">
               <button className="btn btn__secondary navbar-btn">Login</button>
             </Link>
-          </span>
+          )}
         </li>
       </ul>
     </nav>
