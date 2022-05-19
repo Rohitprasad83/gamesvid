9b55f0a77b6d33a432b4d2999dcd875e65febf97
Rohit Prasad <pdrohit674@gmail.com>
2022-05-20 01:02:57 +0530

    feat: added categories on homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import banner from 'assets/images/banner.jpg'
 import { Navbar, Footer } from 'components'
+import axios from 'axios'
+import { HomePageCard } from './HomePageCard'
 export function Home() {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    try {
+      ;(async () => {
+        const response = await axios.get('/api/categories')
+        setCategories(response.data.categories)
+      })()
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
   return (
     <div className="home__container">
       <Navbar />
@@ -11,8 +24,12 @@ export function Home() {
           alt="banner"
           className="responsive__img center__image"
         />
-        <h5 className="text-left home-banner">Trending Videos</h5>
-        <div className="videos-container"></div>
+        <h5 className="text-left home-banner">Categories</h5>
+        <div className="cateogory-container">
+          {categories.map(category => (
+            <HomePageCard key={category.id} category={category} />
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
