f12ef6f90b1f20167ca89a87724f1544ff596f32
Rohit Prasad <pdrohit674@gmail.com>
2022-05-19 23:52:05 +0530

    feat: single playlist video with addvideo to a playlist add playlist

--- src/pages/singlevideo/SingleVideo.jsx
+++ src/pages/singlevideo/SingleVideo.jsx
@@ -2,12 +2,25 @@ import { useState, useEffect } from 'react'
 import { Navbar, Footer, VideoCard } from 'components'
 import { useParams, useNavigate, Link } from 'react-router-dom'
 import { videos } from 'backend/db/videos'
-import { likeVideo, addToWatchLater, addToHistory } from 'services'
+import {
+  likeVideo,
+  addToWatchLater,
+  addToHistory,
+  addPlaylist,
+  deletePlaylistVideo,
+  addVideoToPlaylist,
+} from 'services'
 import { useAuth } from 'context'
+import axios from 'axios'
+
 export function SingleVideo() {
   let { videoId } = useParams()
   const [video, setVideo] = useState({})
-
+  const [playlists, setPlaylists] = useState([])
+  const [openPlaylist, setOpenPlaylist] = useState(false)
+  const [createPlaylist, setCreatePlaylist] = useState(false)
+  const [playlistTitle, setPlaylistTitle] = useState(false)
+  const [playlistDescription, setPlaylistDescription] = useState(false)
   const {
     _id,
     title,
@@ -29,6 +42,20 @@ export function SingleVideo() {
     addToHistory(video)
   }, [videoId])
 
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const response = await axios.get(`/api/user/playlists`, {
+          headers: {
+            authorization: encodedToken,
+          },
+        })
+        setPlaylists(response.data.playlists)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [playlists])
   return (
     <div className="home__container">
       <Navbar />
@@ -57,9 +84,80 @@ export function SingleVideo() {
                   onClick={() => addToWatchLater(video)}>
                   <i className="fa-regular fa-heart"></i>Add to Watch Later
                 </span>
-                <span className="pointer">
+                <span
+                  className="pointer"
+                  onClick={() => setOpenPlaylist(!openPlaylist)}>
                   <i className="fa-solid fa-layer-group"></i>Add to Playist
                 </span>
+                {openPlaylist && (
+                  <div className="playlist">
+                    <div className="text__lg playlist-heading">
+                      <span>Save To</span>
+                      <span>
+                        <i
+                          className="fa-solid fa-x pointer"
+                          onClick={() => {
+                            setOpenPlaylist(false)
+                            setCreatePlaylist(false)
+                          }}></i>
+                      </span>
+                    </div>
+                    <hr />
+                    {playlists.map(playlist => (
+                      <label htmlFor={playlist.title} key={playlist._id}>
+                        <input
+                          type="checkbox"
+                          id={playlist.title}
+                          onChange={() => {
+                            addVideoToPlaylist(
+                              playlist._id,
+                              video,
+                              playlist.title
+                            )
+                            setOpenPlaylist(false)
+                          }}
+                          checked={
+                            playlist.videos.some(video => video._id === _id)
+                              ? true
+                              : false
+                          }
+                        />
+                        {playlist.title}
+                      </label>
+                    ))}
+                    {createPlaylist ? (
+                      <div className="createPlaylist">
+                        <input
+                          type="text"
+                          placeholder="Playlist Title"
+                          onChange={e => setPlaylistTitle(e.target.value)}
+                          required
+                        />
+                        <input
+                          type="text"
+                          placeholder="Playlist Description"
+                          onChange={e => setPlaylistDescription(e.target.value)}
+                          required
+                        />
+                        <div
+                          className="pointer text__center"
+                          onClick={() => {
+                            addPlaylist(playlistTitle, playlistDescription)
+                            setCreatePlaylist(false)
+                          }}>
+                          Create
+                        </div>
+                      </div>
+                    ) : (
+                      <div
+                        className="pointer"
+                        onClick={() => setCreatePlaylist(true)}>
+                        <i className="fa-solid fa-plus"></i> Create a new
+                        playlist
+                      </div>
+                    )}
+                  </div>
+                )}
               </div>
               <div className="video-side-details">
                 <span>{views} views</span>
