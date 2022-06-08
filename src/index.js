ebeee47b95aecf66ef435b4f18253bf72a4750c6
Rohit Prasad <pdrohit674@gmail.com>
2022-06-08 17:31:03 +0530

    fix: react 18 render error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { makeServer } from './server'
 import { BrowserRouter } from 'react-router-dom'
+import * as ReactDOMClient from 'react-dom/client'
 import { AuthContextProvider } from 'context/index'
 import { store } from 'app/store'
 import { Provider } from 'react-redux'
 // Call make Server
 makeServer()
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -19,6 +20,5 @@ ReactDOM.render(
         </AuthContextProvider>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
